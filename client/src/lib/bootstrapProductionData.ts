f462523738b1b0c1f915d0fe9f3fa8b809d7d3fa
recesp-lab <recesp@hotmail.com>
2026-07-13 12:51:28 -0400

    Add bootstrapProductionData function to initialize localStorage

--- client/src/lib/bootstrapProductionData.ts
+++ client/src/lib/bootstrapProductionData.ts
@@ -1,1 +1,11 @@
 
+import { PRODUCTION_BASELINE } from '@/data/productionBaseline';
+
+export function bootstrapProductionData() {
+  Object.entries(PRODUCTION_BASELINE).forEach(([key, value]) => {
+    if (!localStorage.getItem(key)) {
+      localStorage.setItem(key, String(value));
+    }
+  });
+}
+
